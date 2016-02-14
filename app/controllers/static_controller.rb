8216fbc05f49d0477da70e7375d8bb6801391644
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2016-02-14 22:39:22 +0530

    Rendering as json again

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,8 @@
 class StaticController < ApplicationController
   def association
+        json = File.read(Rails.public_path + "association.json")
+        render :json => json, :content_type => "application/pkcs7-mime"
 
-        render :file => Rails.public_path + "association.json", :content_type => "application/pkcs7-mime"
 
   end
 end
